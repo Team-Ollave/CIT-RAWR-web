18c50b1880dfbb13ae611bc3170297e930e37b56
Orlan Quijada <orlantquijada@gmail.com>
2020-11-22 07:33:36 +0800

    styling fix on multiple departments

--- src/screens/ReservationsScreen/Tabs/AcceptedTab.jsx
+++ src/screens/ReservationsScreen/Tabs/AcceptedTab.jsx
@@ -1,41 +1,40 @@
 import Accordion from '../../../components/Accordion';
 import ReservationCard from '../../../components/ReservationCard';
-import { DEPARTMENTS_LIST, ROOMS_LIST } from '../../../data';
 import styles from './styles.module.scss';
 
 export default function AcceptedTab({ forDepartment = false, data }) {
   return (
     <div className={styles.container}>
-      <div className={styles.departmentContainer}>
-        {forDepartment ? (
-          <div className={styles.roomContainer}>
-            {data.map(({ id, roomName, reservations }, index) => (
-              <Accordion key={id} title={roomName} isActive={!index}>
-                <div className={styles.reservationList}>
-                  {reservations.map(
-                    ({
-                      id,
-                      eventName,
-                      eventOrganizerName,
-                      eventStartTime,
-                      eventEndTime,
-                    }) => (
-                      <ReservationCard
-                        key={id}
-                        eventName={eventName}
-                        eventOrganizer={eventOrganizerName}
-                        eventStartTime={eventStartTime}
-                        eventEndTime={eventEndTime}
-                      />
-                    )
-                  )}
-                </div>
-              </Accordion>
-            ))}
-          </div>
-        ) : (
-          data.map(({ departmentName, rooms }) => (
-            <>
+      {forDepartment ? (
+        <div className={styles.roomContainer}>
+          {data.map(({ id, roomName, reservations }, index) => (
+            <Accordion key={id} title={roomName} isActive={!index}>
+              <div className={styles.reservationList}>
+                {reservations.map(
+                  ({
+                    id,
+                    eventName,
+                    eventOrganizerName,
+                    eventStartTime,
+                    eventEndTime,
+                  }) => (
+                    <ReservationCard
+                      key={id}
+                      eventName={eventName}
+                      eventOrganizer={eventOrganizerName}
+                      eventStartTime={eventStartTime}
+                      eventEndTime={eventEndTime}
+                    />
+                  )
+                )}
+              </div>
+            </Accordion>
+          ))}
+        </div>
+      ) : (
+        <div className={styles.departmentListContainer}>
+          {data.map(({ departmentName, rooms }) => (
+            <div className={styles.departmentContainer}>
               <h2 className={styles.departmentName}>{departmentName}</h2>
               <div className={styles.roomContainer}>
                 {rooms.map(({ id, roomName, reservations }, index) => (
@@ -62,10 +61,10 @@ export default function AcceptedTab({ forDepartment = false, data }) {
                   </Accordion>
                 ))}
               </div>
-            </>
-          ))
-        )}
-      </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
