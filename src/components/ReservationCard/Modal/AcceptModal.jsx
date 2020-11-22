a82a016fbc9a85da805103adf55f4dd4c6c87459
Orlan Quijada <orlantquijada@gmail.com>
2020-11-23 04:39:12 +0800

    temporary fix modal close

--- src/components/ReservationCard/Modal/AcceptModal.jsx
+++ src/components/ReservationCard/Modal/AcceptModal.jsx
@@ -1,7 +1,24 @@
+import { useCallback, useEffect } from 'react';
 import Modal from '../../Modal';
 import styles from './styles.module.scss';
 
 export default function AcceptModal({ show, setShow }) {
+  const handleOnEscape = useCallback(
+    ({ key }) => {
+      if (key === 'Escape') {
+        setShow(false);
+      }
+    },
+    [setShow]
+  );
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleOnEscape);
+    return () => {
+      document.removeEventListener('keydown', handleOnEscape);
+    };
+  }, [handleOnEscape]);
+
   return (
     <Modal show={show} setShow={setShow}>
       <div style={{ display: 'flex', columnGap: '1rem' }}>
