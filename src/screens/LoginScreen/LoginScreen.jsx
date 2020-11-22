7b4a9d694cb4816227adaaee0e74e9dcfaac4ba9
Jan Dale Amores <amoresjan14@gmail.com>
2020-11-22 22:28:12 +0800

    added login screen styles

--- src/screens/LoginScreen/LoginScreen.jsx
+++ src/screens/LoginScreen/LoginScreen.jsx
@@ -1,28 +1,30 @@
-import { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { userTypes } from '../../constants';
-import styles from './styles.module.scss';
+import { useState, useContext } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { UserContext } from "../../App";
+import { userTypes } from "../../constants";
+import styles from "./styles.module.scss";
+import icon_logo from "../../assets/icon_logo.png";
 
 const departmentAccount = {
-  username: 'dept',
-  password: '123',
+  username: "dept",
+  password: "123",
   userType: userTypes.DEPARTMENT,
 };
 const imdcAccount = {
-  username: 'imdc',
-  password: '123',
+  username: "imdc",
+  password: "123",
   userType: userTypes.IMDC,
 };
 const presAccount = {
-  username: 'pres',
-  password: '123',
+  username: "pres",
+  password: "123",
   userType: userTypes.PRESIDENT,
 };
 
 export default function LoginScreen() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState("");
   const history = useHistory();
 
   const handleOnSubmit = (event) => {
@@ -32,50 +34,56 @@ export default function LoginScreen() {
       username === departmentAccount.username &&
       password === departmentAccount.password
     ) {
-      history.push('/admin/dashboard');
+      history.push("/admin/dashboard");
     } else if (
       username === imdcAccount.username &&
       password === imdcAccount.password
     ) {
-      history.push('/imdc/dashboard');
+      history.push("/imdc/dashboard");
     } else if (
       username === presAccount.username &&
       password === presAccount.password
     ) {
-      history.push('/president/dashboard');
+      history.push("/president/dashboard");
     }
 
-    setErrors('incorrect username or password');
+    setErrors("incorrect username or password");
   };
 
   return (
     <div className={styles.container}>
-      <form
-        method="post"
-        className={styles.loginFormContainer}
-        onSubmit={handleOnSubmit}
-      >
-        <header>Sign in</header>
-        <div className={styles.bodyContainer}>
-          {errors && <p>{errors}</p>}
-          <input
-            type="text"
-            name="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <div>
-            <Link to="/imdc">Forgot your password?</Link>
-            <button>Sign In</button>
+      <div className={styles.leftPortion}>
+        <img src={icon_logo} alt="logo" className={styles.logo} />
+      </div>
+      <div className={styles.rightPortion}>
+        <div className={styles.signInContainer}>
+          <header className={styles.signInHeader}>Accept Reservations</header>
+          <div className={styles.signInMainContent}>
+            <form method="post" onSubmit={handleOnSubmit}>
+              <input
+                type="text"
+                name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className={styles.signInInputs}
+                placeholder="Email"
+              />
+              <input
+                className={styles.signInInputs}
+                type="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+              />
+              <button className={styles.signInButton}>Sign in</button>
+            </form>
+            <Link className={styles.forgetPasswordButton}>
+              Forgot Password?
+            </Link>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
